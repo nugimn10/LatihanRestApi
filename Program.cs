95f8d2f7d00281760bba93f49dd1c0857035b419
nugimn10 <nugi.mn@gmail.com>
2020-02-13 15:37:25 +0700

    Ngabalakan

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LathianRestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
